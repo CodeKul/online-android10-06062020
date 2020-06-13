ad686195f540e255a588f26039e67ec26cc47d00
aniruddha <aniruddha.kudalkar@gmail.com>
2020-06-13 13:13:48 +0530

    code : intro to coding

--- app/src/main/java/com/melayer/onlineapp/MainActivity.kt
+++ app/src/main/java/com/melayer/onlineapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.melayer.onlineapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        btnOk.setOnClickListener{
+            Log.i("@ani", "Button Clicked")
+
+            val num1 = Integer.parseInt(etNumOne.text.toString())
+            val num2 = Integer.parseInt(etNumTwo.text.toString())
+            txtDt.text = "${num1 + num2}"
+        }
     }
 }
